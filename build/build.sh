fcfaf1a2549c96e3c6ed8ea029285308bcd5f8ff
Bhaskar Pardeshi <biggy.pardeshi@gmail.com>
2020-01-04 18:38:06 +0530

    Removed task.c as it had no code

--- build/build.sh
+++ build/build.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
 # Create the binary directory
-mkdir ../bin
+mkdir -p ../bin
 
 # Compile the source code
-gcc ../lib/task_queue/queue.c ../lib/task/task.c ../lib/scheduler/sch.c ../src/main.c -o ../bin/main
+gcc ../lib/task_queue/queue.c ../lib/scheduler/sch.c ../src/main.c -o ../bin/main
 
